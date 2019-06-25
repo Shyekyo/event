61b0e9ece8198dd6e942abe5c0d140c801d245cc
hyekyo <xiongmao1699@126.com>
2019-06-25 17:42:09 +0800

    add new

--- src/main/scala/ideal/CaseClass.scala
+++ src/main/scala/ideal/CaseClass.scala
@@ -6,16 +6,16 @@ package ideal
 object CaseClass extends Serializable{
   case class ACCOUNT_TYPE(ACCOUNT_TYPE_ID:Int,ACCOUNT_TYPE_NAME:String,ACCOUNT_REF_PREFIX:String,ACCT_TYPE:String) extends Serializable
 
-  case class ACCT_ITEM(ACCT_ITEM_ID:Int,ITEM_SOURCE_ID:Int,CUST_ID:Int,STATE:Int
+    case class ACCT_ITEM(ACCT_ITEM_ID:Int,ITEM_SOURCE_ID:String/**数字可以为空*/,CUST_ID:Int,STATE:Int
                        ,ACCT_ITEM_TYPE_ID:Int,BILLING_CYCLE_ID:Int,CREATE_DATE:Int,
-                       PAYMENT_METHOD:Int,REF_NO:String,GEN_FLAG:Int,GEN_ID:Int,
-                       COMMENTS:String,PAY_TYPE:String,DEP_ABBR:String,FLAG:Int,SETTLE_DATE:Int,
-                       TYPE_ID:Int,BUREAU_ID:Int,APPROVE_FLAG:Int) extends Serializable
+                       PAYMENT_METHOD:String/**数字可以为空*/,REF_NO:String,GEN_FLAG:Int,GEN_ID:Int,
+                       COMMENTS:String,PAY_TYPE:String,DEP_ABBR:String,FLAG:Int,SETTLE_DATE:String/**数字可以为空*/,
+                       TYPE_ID:String/**数字可以为空*/,BUREAU_ID:Int,APPROVE_FLAG:Int) extends Serializable
 
   case class ACCT_ITEM_TYPE(ACCT_ITEM_TYPE_ID:Int,ACCT_ITEM_CLASS_ID:Int,ACCOUNT_TYPE_ID:Int,
                             NAME:String,CHARGE_MARK:Int,TOTAL_MARK:Int,ACCOUNT_ITEM_TYPE_CODE:Int,
                             PAY_FLAG:String,MODIFIER:String,UPDATE_TIME:String) extends Serializable
-  case class AMOUNT(AMOUNT_ID:Int,ACCOUNT_ITEM_ID:Int,STATE:Int,AMOUNT:Int,CURRENCY:String,COMMENTS:String) extends Serializable
+  case class AMOUNT(AMOUNT_ID:Int,ACCOUNT_ITEM_ID:String,STATE:Int,AMOUNT:Double,CURRENCY:String,COMMENTS:String) extends Serializable
 
   case class BILLING_CYCLE(BILLING_CYCLE_ID:Int,CYCLE_BEGIN_DATE:Int,CYCLE_END_DATE:Int) extends Serializable
 
@@ -24,8 +24,8 @@ object CaseClass extends Serializable{
                       PAY_FLAG:String,PAY_TYPE:String,COMMENTS:String,DEP_ABBR:String,
                       PAYMENT_STATE:Int,BUREAU_ID:Int) extends Serializable
 
-  case class STT_OBJECT(CUST_ID:Int,CUST_NAME:String,CUST_TYPE_ID:Integer,CUST_LOCATION:Integer,
-                        IS_VIP:Int,PARENT_ID:Int,CUST_CODE:String,STATE:String,EFF_DATE:String,EXP_DATE:String,
-                        COUNTRY_ID:Int,SCALE:String,MODIFIER:String,UPDATE_TIME:String,TSP_ID:Integer) extends Serializable
+  case class STT_OBJECT(CUST_ID:Int,CUST_NAME:String,CUST_TYPE_ID:Integer,CUST_LOCATION:String/**数字可以为空*/,
+                        IS_VIP:Int,PARENT_ID:String/**数字可以为空*/,CUST_CODE:String,STATE:String,EFF_DATE:String,EXP_DATE:String,
+                        COUNTRY_ID:Int,SCALE:String,MODIFIER:String,UPDATE_TIME:String,TSP_ID:String/**数字可以为空*/) extends Serializable
 
 }
--- src/main/scala/ideal/Rep_total_account_commit.scala
+++ src/main/scala/ideal/Rep_total_account_commit.scala
@@ -1,5 +1,6 @@
 package ideal
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 ;
 
@@ -8,21 +9,22 @@ import org.apache.spark.sql.SparkSession
   */
 object Rep_total_account_commit {
   def main(args: Array[String]): Unit = {
-    var prefix = "D:\\GITRepo\\event\\src\\"
-    prefix = "C:\\Users\\chendd\\Desktop\\张小凡\\LocalRepo\\event\\src\\"
-    val account_type = prefix+"resources\\rep_total_account_commit\\account_type.csv"
-    val acct_item = prefix+"resources\\rep_total_account_commit\\acct_item.csv"
-    val acct_item_type = prefix+"resources\\rep_total_account_commit\\acct_item_type.csv"
-    val amount = prefix+"resources\\rep_total_account_commit\\amount.csv"
-    val billing_cycle = prefix+"src\\resources\\rep_total_account_commit\\billing_cycle.csv"
-    val gen_item = prefix+"resources\\rep_total_account_commit\\gen_item.csv"
-    val stt_object = prefix+"resources\\rep_total_account_commit\\stt_object.csv"
-    var prefix2 = "C:\\Users\\chendd\\Desktop\\张小凡\\LocalRepo\\event\\"
+    Logger.getLogger("org").setLevel(Level.OFF)
+    var prefix = "D:\\GITRepo\\event\\src\\resources"
+    //prefix = "C:\\Users\\chendd\\Desktop\\张小凡\\LocalRepo\\event\\src\\"
+    val account_type = prefix+"\\rep_total_account_commit\\account_type.csv"
+    val acct_item = prefix+"\\rep_total_account_commit\\acct_item.csv"
+    val acct_item_type = prefix+"\\rep_total_account_commit\\acct_item_type.csv"
+    val amount = prefix+"\\rep_total_account_commit\\amount.csv"
+    val billing_cycle = prefix+"\\rep_total_account_commit\\billing_cycle.csv"
+    val gen_item = prefix+"\\rep_total_account_commit\\gen_item.csv"
+    val stt_object = prefix+"\\rep_total_account_commit\\stt_object.csv"
+    //var prefix2 = "C:\\Users\\chendd\\Desktop\\张小凡\\LocalRepo\\event\\"
     val spark = SparkSession.builder().
       appName("xsql").
       master("local[1]").
-      //config("spark.sql.warehouse.dir", "D:/GITRepo/event/spark-warehouse").
-      config("spark.sql.warehouse.dir", prefix2+"spark-warehouse").
+      config("spark.sql.warehouse.dir", "D:/GITRepo/event/spark-warehouse").
+      //config("spark.sql.warehouse.dir", prefix2+"spark-warehouse").
       getOrCreate()
     import spark.implicits._
     //val account_type_df = spark.read.csv(account_type).as[ccl.ACCOUNT_TYPE]
@@ -34,19 +36,30 @@ object Rep_total_account_commit {
     //val gen_item_df = spark.read.csv(gen_item).as[ccl.GEN_ITEM]
     //val stt_object_df = spark.read.csv(stt_object).as[ccl.STT_OBJECT]
     val sc = spark.sparkContext
+
     val account_type_df = sc.textFile(account_type).map(
       line => {
         val arr = line.split(",")
         CaseClass.ACCOUNT_TYPE(arr(0).toInt,arr(1),arr(2),arr(3))
       }).toDS()
+
+
     val acct_item_df = sc.textFile(acct_item).map(
       line => {
         var rline = line.replace("\"","")
-        val arr = rline.split(",")
-        CaseClass.ACCT_ITEM(arr(0).toInt, arr(1).toInt, arr(2).toInt, arr(3).toInt, arr(4).toInt, arr(5).toInt,
-          arr(6).toInt, arr(7).toInt, arr(8), arr(9).toInt, arr(10).toInt, arr(11), arr(12), arr(13),
-          arr(14).toInt, arr(15).toInt, arr(16).toInt, arr(17).toInt, arr(18).toInt)
+        val arr = rline.split("\\|")
+        if(arr.length==19){
+          CaseClass.ACCT_ITEM(arr(0).toInt, arr(1), arr(2).toInt, arr(3).toInt, arr(4).toInt, arr(5).toInt,
+            arr(6).toInt, arr(7), arr(8), arr(9).toInt, arr(10).toInt, arr(11), arr(12), arr(13),
+            arr(14).toInt, arr(15), arr(16), arr(17).toInt, arr(18).toInt)
+        }else{
+          CaseClass.ACCT_ITEM(arr(0).toInt, arr(1), arr(2).toInt, arr(3).toInt, arr(4).toInt, arr(5).toInt,
+            arr(6).toInt, arr(7), arr(8), arr(9).toInt, arr(10).toInt, arr(11), arr(12), arr(13),
+            arr(14).toInt, arr(15), arr(16), arr(17).toInt, 0)
+        }
       }).toDS()
+
+
     val acct_item_type_df = sc.textFile(acct_item_type).map(
       line => {
         var rline = line.replace("\"","")
@@ -54,18 +67,30 @@ object Rep_total_account_commit {
         CaseClass.ACCT_ITEM_TYPE(arr(0).toInt, arr(1).toInt, arr(2).toInt, arr(3), arr(4).toInt, arr(5).toInt,
           arr(6).toInt, arr(7), arr(8), arr(9))
       }).toDS()
+
+
     val amount_df = sc.textFile(amount).map(
       line => {
         var rline = line.replace("\"","")
         val arr = rline.split(",")
-        CaseClass.AMOUNT(arr(0).toInt, arr(1).toInt, arr(2).toInt, arr(3).toInt, arr(4), arr(5))
+        if(arr.length==6){
+          CaseClass.AMOUNT(arr(0).toInt, arr(1), arr(2).toInt, arr(3).toDouble, arr(4), arr(5))
+        }else{
+          CaseClass.AMOUNT(arr(0).toInt, arr(1), arr(2).toInt, arr(3).toDouble, arr(4), "")
+        }
       }).toDS()
+
+
+
     val billing_cycle_df = sc.textFile(billing_cycle).map(
       line => {
         var rline = line.replace("\"","")
         val arr = rline.split(",")
         CaseClass.BILLING_CYCLE(arr(0).toInt, arr(1).toInt, arr(2).toInt)
       }).toDS()
+
+
+
     val gen_item_df = sc.textFile(gen_item).map(
       line => {
         var rline = line.replace("\"","")
@@ -74,13 +99,24 @@ object Rep_total_account_commit {
           arr(6).toInt, arr(7).toInt, arr(8).toInt, arr(9), arr(10), arr(11), arr(12),
           arr(13).toInt,arr(14).toInt)
       }).toDS()
+
+
+
     val stt_object_df = sc.textFile(stt_object).map(
       line => {
         var rline = line.replace("\"","")
-        val arr = rline.split(",")
-        CaseClass.STT_OBJECT(arr(0).toInt, arr(1), arr(2).toInt, arr(3).toInt, arr(4).toInt, arr(5).toInt,
-          arr(6), arr(7), arr(8), arr(9), arr(10).toInt, arr(11), arr(12),arr(13),arr(14).toInt)
+        val arr = rline.split("\\|")
+        if(arr.length==15) {
+          CaseClass.STT_OBJECT(arr(0).toInt, arr(1), arr(2).toInt, arr(3), arr(4).toInt, arr(5),
+            arr(6), arr(7), arr(8), arr(9), arr(10).toInt, arr(11), arr(12), arr(13), arr(14))
+        }else{
+          CaseClass.STT_OBJECT(arr(0).toInt, arr(1), arr(2).toInt, arr(3), arr(4).toInt, arr(5),
+            arr(6), arr(7), arr(8), arr(9), arr(10).toInt, arr(11), arr(12), arr(13), "")
+        }
       }).toDS()
+
+
+
     account_type_df.createOrReplaceTempView("account_type")
     acct_item_df.createOrReplaceTempView("acct_item")
     acct_item_type_df.createOrReplaceTempView("acct_item_type")
@@ -88,45 +124,27 @@ object Rep_total_account_commit {
     billing_cycle_df.createOrReplaceTempView("billing_cycle")
     gen_item_df.createOrReplaceTempView("gen_item")
     stt_object_df.createOrReplaceTempView("stt_object")
-    /**
-    val sql = "select " +
-      "g.gen_id," +
-      "g.ref_no," +
-      "stt.cust_name," +
-      "ait.acct_item_type_id," +
-      "ait.name," +
-      "bc.cycle_begin_date," +
-      "bc.cycle_end_date," +
-      "a.amount," +
-      "a.currency," +
-      "ait.pay_flag," +
-      "g.start_date," +
-      "g.end_date,decode(decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type), 'TELEPHONE', 1," +
-      "decode(decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type), 'TELX', 2," +
-      "decode(decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type), 'TELEGRAM', 3," +
-      "decode(decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type),'COMMITMENT ADJUST', 5," +
-      "decode(decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type),ai.comments, 6, 4))))) sort_id," +
-      "decode(sign(instr(ait.name, '调整')), 1, ai.comments, at.acct_type) acct_name," +
-      "g.acct_date " +
-      "from gen_item g," +
-      "acct_item_type ait," +
-      "acct_item ai," +
-      "amount a," +
-      "stt_object stt," +
-      "billing_cycle bc," +
-      "account_type at" +
-      "where " +
-      "a.account_item_id = ai.acct_item_id " +
+    /*val sql = "select g.gen_id,g.ref_no,stt.cust_name,ait.acct_item_type_id,ait.name,bc.cycle_begin_date,bc.cycle_end_date," +
+      "a.amount,a.currency,ait.pay_flag,g.start_date,g.end_date," +
+      "decode(decode((case when instr(ait.name, '调整')==1 then 1 else -1 end), 1, ai.comments, at.acct_type), 'TELEPHONE', 1," +
+      "decode(decode((case when instr(ait.name, '调整')==1 then 1 else -1 end), 1, ai.comments, at.acct_type), 'TELX', 2," +
+      "decode(decode((case when instr(ait.name, '调整')==1 then 1 else -1 end), 1, ai.comments, at.acct_type), 'TELEGRAM', 3," +
+      "decode(decode((case when instr(ait.name, '调整')==1 then 1 else -1 end), 1, ai.comments, at.acct_type), 'COMMITMENT ADJUST', 5," +
+      "decode(decode((case when instr(ait.name, '调整')==1 then 1 else -1 end), 1, ai.comments, at.acct_type),ai.comments, 6, 4)))))  sort_id," +
+      "decode((case when instr(ait.name, '调整')==1 then 1 else -1 end),1, ai.comments, at.acct_type) acct_name,g.acct_date " +
+      "from gen_item g,acct_item_type ait,acct_item ai,amount a, stt_object stt,billing_cycle bc,account_type at" +
+      "where a.account_item_id = ai.acct_item_id " +
       "and ai.acct_item_type_id = ait.acct_item_type_id " +
-      "and ai.gen_id = g.gen_id and g.cust_id = stt.cust_id " +
+      "and ai.gen_id = g.gen_id  " +
+      "and g.cust_id = stt.cust_id " +
       "and at.account_type_id =decode(ait.account_type_id, 59, 47, 60, 48, 61, 49, 71, 69, 70, 68, ait.account_type_id)" +
       "and ai.billing_cycle_id = bc.billing_cycle_id " +
       "and g.bureau_id = ai.bureau_id " +
-      "and g.bureau_id = bureauId " +
-      "and g.ref_no = 1;"
-      *
-      */
-    val sql ="select * from acct_item ai"//,amount a where a.account_item_id = ai.acct_item_id "
+      "and g.bureau_id = 1 " +
+      "and g.ref_no = 1;"*/
+    val sql ="select * from acct_item ai,amount a where a.account_item_id = ai.acct_item_id "
+    //val sql = "select * from amount"
     spark.sql(sql).show()
+    spark.stop()
   }
 }
